4ce3234e6e8028cf15848cb1c99c1d781aef3593
Rononoa13 <sumitluitel42@gmail.com>
2019-04-03 15:28:52 +0545

    Sitemap links into lists and csv file

--- aanapurna_scrape.py
+++ aanapurna_scrape.py
@@ -1,20 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import pandas as pd
 
 
 
 page_response = requests.get('http://annapurnapost.com/sitemap.xml')
 soup =  BeautifulSoup(page_response.content, 'xml')
 
+
 output_url = []
-print(soup.prettify())
+
+#print(soup.prettify())
+
+for date in soup.find_all('lastmod'):
+    print(date.text)
+
+
+
 for url in soup.find_all('loc'):
     output_url.append(url.text)
-    print(url.text)
 
-with open('url_output.text', 'a') as f:
-    f.write("%s\n" % output_url)
-    print("\n")
+#print(output_url)
+# create new dataframe
+
+dataframe = pd.DataFrame(output_url)
+#print(dataframe)
+dataframe.to_csv(r'/home/ed_666/Documents/Project_Scrape/aanapurna_dailyLink.csv', index = False)
 
 
+    
